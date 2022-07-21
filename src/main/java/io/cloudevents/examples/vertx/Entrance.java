c417d7fadb2e484ac13b34fd1503108fd91250b2
sling <18102030131@163.com>
2022-07-21 11:20:56 +0800

    第二次版本提交
    Signed-off-by: sling <18102030131@163.com>

--- src/main/java/io/cloudevents/examples/vertx/Entrance.java
+++
@@ -1,9 +0,0 @@
-package io.cloudevents.examples.vertx;
-
-import com.linkall.vance.core.VanceApplication;
-
-public class Entrance {
-    public static void main(String[] args) {
-        VanceApplication.run(GitHubHttpSource.class);
-    }
-}
